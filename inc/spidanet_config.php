95e9366f07bad839a794702e5a3ec806e95dca28
sapangupta2009 <33867276+sapangupta2009@users.noreply.github.com>
2017-11-30 20:04:06 +0530

    First Commit

---
+++ inc/spidanet_config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Spidanet subscribers configuration data.
+ */
+global $spidanet_restaurants;
+$spidanet_restaurants = array(
+	array( 
+		"id" => 1,
+		"name" => "Junoon Indian Restaurant",
+		"email" => "patel.dipenk@gmail.com",
+		"status" => "Live",
+		"db_name" => "yourfood_junoonsysdb",
+		"db_user" => "yourfood_junusr",
+		"db_pass" => "iEN-ObIk~4eQ",
+		"db_host" => "localhost",
+	),
+);
